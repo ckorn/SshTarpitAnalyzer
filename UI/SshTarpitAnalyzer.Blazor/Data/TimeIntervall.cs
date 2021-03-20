65c59aa593d2d44f6f182bc226adeac9741a86af
Christoph Korn <christoph.korn@posteo.de>
2021-03-20 13:17:59 +0100

    Make grouping more flexibel
    Group by seconds, minutes or hours. Let the user decide the value.

---
+++ UI/SshTarpitAnalyzer.Blazor/Data/TimeIntervall.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UI.SshTarpitAnalyzer.Blazor.Data
+{
+    public enum TimeIntervall
+    {
+        Seconds,
+        Minutes,
+        Hours
+    }
+}
